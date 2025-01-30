cd627ac046b7a7631a2ab20a587d30f67af51550
NISHANTH-SYNECX <nishanthkr@synecxailabs.io>
2025-01-30 15:27:54 +0530

    done

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,7 @@
 import Blogs from '@/components/landing-page/blog/blogs'
 import React from 'react'
-
+import { Metadata } from "next";
+export const metadata = { title: "SYNECX | BLOGS " } as Metadata;
 const page = () => {
   return (
     <div><Blogs /></div>
--- src/components/landing-page/blog/blogs.tsx
+++ src/components/landing-page/blog/blogs.tsx
@@ -1,5 +1,4 @@
 "use client";
-import SnowParticles from "@/components/ParticlsJs/snowparticles";
 import Navbar from "@/layouts/navbar";
 import React from "react";
 
@@ -8,9 +7,7 @@ const Blogs = () => {
    <div>
      <Navbar />
      <div className=" flex justify-center items-center h-screen">
-      <div className="absolute inset-0 -z-10">
-        <SnowParticles id="particles-js" />
-      </div>
+      
     </div>
    </div>
   );
